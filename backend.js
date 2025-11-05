0f48fd0c4b1b7819ee86ee902045cec8df02c634
Techy <praisejiro43210@gmail.com>
2025-11-05 15:15:24 +0100

    Update backend.js

--- backend.js
+++ backend.js
@@ -23,11 +23,13 @@ app.use(express.static(path.join(__dirname)));
 
 // ===== Email transporter =====
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: 'smtp.gmail.com',
+  port: 465, // SSL port
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
 
 // ===== ROUTES =====
